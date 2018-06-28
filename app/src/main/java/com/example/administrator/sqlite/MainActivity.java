28fb8be32f4b7066c4ad9fce0797c2398e976764
yuanjuan <juanyuan.zhang@gmail.com>
2018-06-28 16:45:15 +0800

    2018.06.28 SQLite

---
+++ app/src/main/java/com/example/administrator/sqlite/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.administrator.sqlite;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.SQLData;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText editName, editPhone, editEmail, editQuery;
+    Button btnAdd, btnAlter, btnDel, btnQuery;
+    TextView tvName, tvPhone, tvEmail;
+    private SQLiteDatabase db;
+    private String TABLE_NAME = " myConnect";  //自訂資料庫名稱
+    private String sql;
+    private String newName,newPhone,newEmail;
+    private int id;   //判斷用
+    private String keyName;//修改用
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        open();//打開資料連結
+        creatTable(); //建立資料庫
+    }
+
+    public void open(){
+        String path = "/data/data/" +getPackageName() +"/contacts.db";
+        db = SQLiteDatabase.openOrCreateDatabase(path,null);
+        Log.i("hihihi","hohoho");
+    }
+    public void creatTable(){  //if not exists如果沒有資料庫才建立TABLE
+        sql = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME +
+                "(_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, phone TEXT, email TEXT);"; //最後記得分號
+        db.execSQL(sql);
+        Log.i("sql",sql);
+    }
+    public void add(){ //add新增資料
+        newName = editName.getText().toString();
+        newPhone = editPhone.getText().toString();
+        newEmail = editEmail.getText().toString();
+        sql = "INSERT INTO "+TABLE_NAME+"(name,phone,email)"+"VALUES(?,?,?)";  //sql語法-寫法1
+        try{
+            db.execSQL(sql, new Object[]{newName,newPhone,newEmail});
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            Toast.makeText(this,"您新增一筆資料",Toast.LENGTH_LONG).show();
+            editName.setText("");
+            editPhone.setText("");
+            editEmail.setText("");
+        }
+    }
+    private void query(){
+        keyName = editQuery.getText().toString();
+        sql ="SELECT * FROM "+TABLE_NAME+" WHERE name = ? ";
+        Cursor cursor = db.rawQuery(sql,new String[]{keyName});
+        if(cursor.getCount()>0){
+            while (cursor.moveToNext()){ //Cursor開始值為-1 ，所以需要使用moveToNext()方法，使其道下一位0
+                id=cursor.getInt(0);
+                editName.setText(cursor.getString(1));
+                editPhone.setText(cursor.getString(2));
+                editEmail.setText(cursor.getString(3));
+                tvName.setText(cursor.getString(1));
+                tvPhone.setText(cursor.getString(2));
+                tvEmail.setText(cursor.getString(3));
+            }
+        }else{
+            Toast.makeText(this,"查無此資料",Toast.LENGTH_LONG).show();
+        }
+        cursor.close(); //關閉
+    }
+    private void alter(){
+        newName = editName.getText().toString();
+        newPhone = editPhone.getText().toString();
+        newEmail = editEmail.getText().toString();
+        sql = "UPDATE "+TABLE_NAME+" SET name = '"+newName+"', phone= '"+newPhone+"', email= '"+newEmail+"'  where _id = ?" ;
+        try {
+            db.execSQL(sql, new String[]{String.valueOf (id)});
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            Log.i("sql",sql);
+            Toast.makeText(this,"修改成功!",Toast.LENGTH_SHORT).show();
+            editName.setText("");
+            editPhone.setText("");
+            editEmail.setText("");
+        }
+
+    }
+    private void delete(){
+        sql = "DELETE FROM "+TABLE_NAME+" WHERE _id=? ";
+        try {
+            db.execSQL(sql, new String[]{String.valueOf(id)});
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            Toast.makeText(this,"刪除成功!",Toast.LENGTH_SHORT).show();
+            editName.setText("");
+            editPhone.setText("");
+            editEmail.setText("");
+            tvName.setText("");
+            tvPhone.setText("");
+            tvEmail.setText("");
+        }
+
+    }
+    private void initView(){
+        editName =findViewById(R.id.name);
+        editPhone =findViewById(R.id.phone);
+        editEmail =findViewById(R.id.email);
+        editQuery =findViewById(R.id.search);
+        btnAdd = findViewById(R.id.btnAdd);
+        btnAlter =findViewById(R.id.btnEdit);
+        btnDel = findViewById(R.id.btnDel);
+        btnQuery = findViewById(R.id.btnQuery);
+        tvName =findViewById(R.id.showName);
+        tvPhone =findViewById(R.id.showPhone);
+        tvEmail =findViewById(R.id.showMail);
+
+        btnAdd.setOnClickListener(this);
+        btnAlter.setOnClickListener(this);
+        btnDel.setOnClickListener(this);
+        btnQuery.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnAdd:
+                add();
+                break;
+            case R.id.btnEdit:
+                alter();
+                break;
+            case R.id.btnDel:
+                delete();
+                break;
+            case R.id.btnQuery:
+                query();
+                break;
+        }
+    }
+}
